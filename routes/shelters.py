a93d0da788dfdd949464aa6154eab0025fd67ef1
skayaaa <skya1903@gmail.com>
2024-12-07 17:20:16 +0300

    cached

--- routes/shelters.py
+++ routes/shelters.py
@@ -1,12 +1,19 @@
 from sanic import Blueprint, response
 from models.shelters import Shelter
 from sanic.response import json
+from aiocache import cached , caches
 
 shelters_bp = Blueprint("shelters", url_prefix="/shelters")
+@cached(ttl=60, key = "shelter_list")
+async def get_shelters_list():
+        shelters = await Shelter.all().values("id","name","location")
+        return shelters 
 
 @shelters_bp.route("/add", methods=["POST"])
 async def add_shelter(request):
     data = request.json
+    if not data:
+        return json({"error" : "Request must be JSON"}, status = 400)
     name = data.get("name")
     location = data.get("location")
 
@@ -14,6 +21,9 @@ async def add_shelter(request):
         return json({"error": "Name and location are required"}, status=400)
 
     shelter = await Shelter.create(name=name, location=location)
+    cache = caches.get("default")
+    await cache.delete("shelter_list")
+    
     return json({"message": "Shelter added successfully", "shelter": {
         "id": shelter.id, "name": shelter.name, "location": shelter.location
     }}, status=201)
@@ -43,6 +53,8 @@ async def update_shelter(request, shelter_id):
     shelter.name = data.get("name", shelter.name)
     shelter.location = data.get("location", shelter.location)
     await shelter.save()
+    cache = caches.get("default")
+    cache = caches.delete("shelter_list")
 
     return json({"message": "Shelter updated successfully", "shelter": {
         "id": shelter.id, "name": shelter.name, "location": shelter.location
@@ -54,6 +66,9 @@ async def delete_shelter(request, shelter_id):
 
     if not shelter:
         return json({"error": "Shelter not found"}, status=404)
+    
+    cache = caches.get("default")
+    cache = caches.delete("shelter_list")
 
     await shelter.delete()
     return json({"message": "Shelter deleted successfully"}, status=200)
